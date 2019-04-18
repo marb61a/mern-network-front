277d14be290df561fc1d4cdaf1692c7ab7cab86e
marb61a <marb61a@gmail.com>
2019-04-18 11:45:11 +0100

    added apiPost create and singlePost functions

---
+++ src/post/apiPost.js
@@ -0,0 +1,24 @@
+export const create = (userId, token, post) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/post/new/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: post
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const singlePost = postId => {
+  return fetch(`${process.env.REACT_APP_API_URL}/post/${postId}`, {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};

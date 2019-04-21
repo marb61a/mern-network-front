d1f978f0d427e336c94bf071ff6b56e8a9756aec
marb61a <marb61a@gmail.com>
2019-04-21 11:50:35 +0100

    added Home and updated Menu core files

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
-const Menu = () => (
+const isActive = (history, path) => {
+  if(history.location.pathname === path) {
+    return {color: "ff9900"};
+  } else {
+    return { color: "#ffffff" };
+  }
+};
+
+const Menu = ({history}) => (
   <div>
     <ul className="nav nav-tabs bg-primary">
       <li className="nav-item">
-        <Link>
+        <Link
+          className="nav-link"
+          style={isActive(history, "/")}
+          to="/"
+        >
           Home
         </Link>
       </li>

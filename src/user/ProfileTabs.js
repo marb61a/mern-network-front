dfbca39ed361b7f2e59c3a7229d5d2a8337300ad
marb61a <marb61a@gmail.com>
2019-04-21 20:27:46 +0100

    added initial ProfileTabs.js

---
+++ src/user/ProfileTabs.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import DefaultProfile from "../images/avatar.jpg";
+
+class ProfileTabs extends Component {
+  const { posts } = this.props;
+  
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-4">
+        
+        </div>
+        <div className="col-md-4">
+        
+        </div>
+        <div className="col-md-4">
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileTabs;

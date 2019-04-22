ddddbd477c9fce386eac11046cdc38363c427f91
marb61a <marb61a@gmail.com>
2019-04-22 12:03:19 +0100

    added signup and signin functions

---
+++ src/auth/index.js
@@ -0,0 +1,29 @@
+export const signup = user => {
+  return fetch(`${process.env.REACT_APP_API_URL}/signup`, {
+    method: "POST",
+    header: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const signin = user => {
+  return fetch(`${process.env.REACT_APP_API_URL}/signin`, {
+    method: "POST",
+    header: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};

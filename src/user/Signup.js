1b0eded73e1fdf56ee2224a0d8b9f9f2bfe25597
marb61a <marb61a@gmail.com>
2019-04-22 11:47:14 +0100

    updated Signup form

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -13,11 +13,42 @@ class Signup extends Component {
     }
   }
 
+  handleChange = name => event => {
+    this.setState({
+      error: ""
+    });
+
+    this.setState({ 
+      [name]: event.target.value 
+    });
+  }; 
+
+  signupForm = (name, email, password) => (
+    <form>
+      <div className="form-group">
+        <input
+          onChange={this.handleChange("name")}
+          type="text"
+          className="form-control"
+          value={name}
+        />
+      </div>
+    </form>
+  );
+
   render() {
+    const { email, name, password, error } = this.state;
+
     return(
       <div className="container">
         <h2 className="mt-5 mb-5">Signup</h2>
-        
+        <hr />
+        <div
+          className="alert alert-danger"
+          style={{display: error ? "" : "none" }}
+        >
+          {error}
+        </div>
       </div>
     );
   }

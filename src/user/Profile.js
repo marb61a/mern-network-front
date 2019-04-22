7458786c95cf56920f5bb324a35552051e564208
marb61a <marb61a@gmail.com>
2019-04-22 12:12:48 +0100

    updated Profile.js

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,21 +1,45 @@
 import React, { Component } from "react";
 import { Redirect, Link } from "react-router-dom";
 
+import { isAuthenticated } from "../auth";
+import ProfileTabs from "./ProfileTabs";
+import { read } from "./apiUser";
+
 class Profile extends Component {
   constructor() {
     super();
     this.state = {
       error: "",
+      redirectToSignin: false,
       posts: []
 
     };
   }
 
-  componentWillMount() {
-    
+  init = userId => {
+    const token = isAuthenticated().token;
+    read(userId, token)
+      .then(data => {
+        if(data.error) {
+          this.setState({ 
+            redirectToSignin: true 
+          });
+        } else {
+          this.setState({
+            user: data,
+          });
+        }
+      });
+  };
+
+  componentDidMount() {
+    const userId = this.props.match.params.userId;
+    this.init(userId);
   }
 
   render() {
+    const {user, posts} = this.state;
+
     return(
       <div className="container">
         <h2 className="mt-5 mb-5">Profile</h2>

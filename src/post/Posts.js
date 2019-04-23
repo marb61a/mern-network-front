71b73b9680ff8dcb187c54ae5892c6e24706008d
marb61a <marb61a@gmail.com>
2019-04-23 12:11:57 +0100

    added renderPost function to Posts.js

--- src/post/Posts.js
+++ src/post/Posts.js
@@ -28,21 +28,49 @@ class Posts extends Component {
 
   componentDidMount() {
     this.loadPosts(this.state.page);
-}
+  };
 
-loadMore = number => {
+  loadMore = number => {
     this.setState({ 
       page: this.state.page + number 
     });
     this.loadPosts(this.state.page + number);
-};
+  };
 
-loadLess = number => {
+  loadLess = number => {
     this.setState({ 
       page: this.state.page - number 
     });
     this.loadPosts(this.state.page - number);
-};
+  };
+
+  renderPosts = posts => {
+    return(
+      <div className="row">
+        {
+          posts.map((post, i) => {
+            const posterId = post.postedBy ? 
+            `/user/${post.postedBy._id}` : 
+            "";
+            const posterName = post.postedBy ? 
+            post.postedBy.name : 
+            " Unknown";
+
+            return(
+              <div className="card col-md-4" key={i}>
+                <div className="row">
+                  <img 
+                    src={}
+                    alt={post.title}
+                  />
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 
   render() {
     const { posts, page } = this.state;

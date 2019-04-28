472379cc19babda6049929ff354ecedebb33d866
marb61a <marb61a@gmail.com>
2019-04-28 11:30:05 +0100

    updated newPost functions

--- src/post/NewPost.js
+++ src/post/NewPost.js
@@ -10,13 +10,40 @@ class NewPost extends Component {
       title: "",
       body: "",
       error: "",
-      user: ""
+      user: "",
+      loading: false
     };
   }
 
   componentDidMount() {
-
+    this.postData = new FormData();
+    this.setState({
+      user: isAuthenticated().user
+    });
   }
+
+  isValid = () => {
+    const { title, body, fileSize } = this.state;
+
+    if(fileSize > 100000) {
+      this.setState({
+        error: "File size should be less than 100kb",
+        loading: false
+      });
+
+      return false;
+    }
+
+    if (title.length === 0 || body.length === 0) {
+      this.setState({ 
+        error: "All fields are required", 
+        loading: false 
+      });
+      return false;
+    }
+
+    return true;
+  };
 
   newPostForm = (title, body) => (
     <form>

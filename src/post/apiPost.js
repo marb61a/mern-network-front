8ba55a3b07cf41368147703d832d5c59339ad4b0
marb61a <marb61a@gmail.com>
2019-04-29 20:06:53 +0100

    updated apiPost commenting methods

--- src/post/apiPost.js
+++ src/post/apiPost.js
@@ -69,3 +69,34 @@ export const update = (postId, token, post) => {
     .catch(err => console.log(err));
 };
 
+export const comment = (userId, token, postId, comment) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/post/comment`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({ userId, postId, comment })
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const uncomment = (userId, token, postId, comment) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/post/uncomment`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({ userId, postId, comment })
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};

d50c5203038cc4a90864286f09c6bfb381236348
marb61a <marb61a@gmail.com>
2019-05-05 20:33:53 +0100

    added updateUser and follow methods to apiUser

--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -39,3 +39,31 @@ export const list = () => {
   })
   .catch(err => console.log(err));
 };
+
+export const updateUser = (user, next) => {
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("jwt")) {
+      let auth = JSON.parse(localStorage.getItem("jwt"));
+      auth.user = user;
+      
+      localStorage.setItem("jwt", JSON.stringify(auth));
+      next();
+    }
+  }
+};
+
+export const follow = (userId, token, followId) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/user/follow`, {
+    method: "PUT",
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({ userId, followId })
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};

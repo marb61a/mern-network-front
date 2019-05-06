fd0b7d6224afd283a4c0c8307e383487204c12f0
marb61a <marb61a@gmail.com>
2019-05-06 11:41:21 +0100

    updated signup.js

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import { signup } from "../auth";
+import SocialLogin from "./SocialLogin";
 
 class Signup extends Component {
   constructor() {
@@ -11,7 +12,8 @@ class Signup extends Component {
       email: "",
       password: "",
       error: "",
-      
+      open: false,
+      recaptcha: false
     };
   }
 
@@ -24,6 +26,36 @@ class Signup extends Component {
       [name]: event.target.value 
     });
   };
+
+  recaptchaHandler = e => {
+    this.setState({
+      error: ""
+    });
+    let userDay = e.target.value.toLowerCase();
+    let dayCount;
+
+    if(userDay === 'sunday') {
+      dayCount = 0;
+    } else if(userDay === 'monday') {
+      dayCount = 1;
+    } else if(userDay === 'tuesday') {
+      dayCount = 2;
+    } else if (userDay === "wednesday") {
+      dayCount = 3;
+    } else if (userDay === "thursday") {
+      dayCount = 4;
+    } else if (userDay === "friday") {
+      dayCount = 5;
+    } else if (userDay === "saturday") {
+      dayCount = 6;
+    }
+
+    if (dayCount === new Date().getDay()) {
+      this.setState({
+        recaptcha: true
+      });
+    }
+  }
 
   clickSubmit = event => {
     event.preventDefault();

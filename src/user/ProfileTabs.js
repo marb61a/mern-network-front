6db933ac5e2afe1cdb69f8fcb709cbfbd637fe7d
marb61a <marb61a@gmail.com>
2019-05-12 19:33:51 +0100

    Updated ProfileTabs render method

--- src/user/ProfileTabs.js
+++ src/user/ProfileTabs.js
@@ -55,10 +55,28 @@ class ProfileTabs extends Component {
             following.map((person, i) => (
               <div key={i}>
                 <div>
-                  <Link  to={`/user/${person._id}`}>
+                  <Link to={`/user/${person._id}`}>
                     <img 
-                    
+                      style={{
+                        borderRadius: "50%",
+                        border: "1px solid black"
+                      }}
+                      className="float-left mr-2"
+                      height="30px"
+                      width="30px"
+                      onError={i =>
+                        (i.target.src = `${DefaultProfile}`)
+                      }
+                      src={`${
+                        process.env.REACT_APP_API_URL
+                      }/user/photo/${person._id}`}
+                      alt={person.name}
                     />
+                    <div>
+                      <p className="lead">
+                        {person.name}
+                      </p>
+                    </div>
                   </Link>
                 </div>
               </div>
@@ -66,7 +84,23 @@ class ProfileTabs extends Component {
           }
         </div>
         <div className="col-md-4">
-        
+          <h3 className="text-primary">
+            {posts.length}
+          </h3>
+          <hr />
+          {
+            posts.map((post, i) => (
+              <div key={i}>
+                <div>
+                  <Link to={`/post/${post._id}`}>
+                    <div>
+                    <p className="lead">{post.title}</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>

c77212cb6be4149c729b1a06ce02cbcdc3cce890
marb61a <marb61a@gmail.com>
2019-05-12 19:27:40 +0100

    Updated ProfileTabs render method

--- src/user/ProfileTabs.js
+++ src/user/ProfileTabs.js
@@ -26,7 +26,20 @@ class ProfileTabs extends Component {
                       className="float-left mr-2"
                       height="30px"
                       width="30px"
+                      onError={i => 
+                        (i.target.src = `${DefaultProfile}`)
+                      }
+                      src={`${
+                        process.env.REACT_APP_API_URL
+                        }/user/photo/${person._id}`
+                      }
+                      alt={person.name}
                     />
+                    <div>
+                      <p className="lead">
+                        {person.name}
+                      </p>
+                    </div>
                   </Link>
                 </div>
               </div>
@@ -34,7 +47,23 @@ class ProfileTabs extends Component {
           }
         </div>
         <div className="col-md-4">
-        
+          <h3 className="text-primary">
+            {following.length} Following
+          </h3>
+          <hr />
+          {
+            following.map((person, i) => (
+              <div key={i}>
+                <div>
+                  <Link  to={`/user/${person._id}`}>
+                    <img 
+                    
+                    />
+                  </Link>
+                </div>
+              </div>
+            ))
+          }
         </div>
         <div className="col-md-4">
         

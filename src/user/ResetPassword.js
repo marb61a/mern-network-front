6ab3660847f9072cbbac95224fa8070fc794990e
marb61a <marb61a@gmail.com>
2019-05-13 17:49:28 +0100

    Updated resetPassword method in ResetPassword.js

--- src/user/ResetPassword.js
+++ src/user/ResetPassword.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import { resetPassword } from "../auth";
 
 class ResetPassword extends Component {
@@ -17,6 +18,26 @@ class ResetPassword extends Component {
       message: "",
       error: ""
     });
+
+    resetPassword({
+      newPassword: this.state.newPassword,
+      resetPasswordLink: this.props.match.params.resetPasswordToken
+    })
+      .then(data => {
+        if(data.error) {
+          console.log(data.error);
+          this.setState({
+            error: data.error,
+            newPassword: ""
+          });
+        } else {
+          console.log(data.message);
+          this.setState({
+            message: data.message,
+            newPassword: ""
+          });
+        }
+      });
   };
 
   render() {
@@ -24,7 +45,11 @@ class ResetPassword extends Component {
       <div className="container">
         <h2 className="mt-5 mb-5">Reset your Password</h2>
         {
-
+          this.state.message && (
+            <h4 className="bg-success">
+              {this.state.message}
+            </h4>
+          )
         }
       </div>
     );

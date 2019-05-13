58e4a938c2b44a594eb05b7c4521529eb04fc085
marb61a <marb61a@gmail.com>
2019-05-13 18:11:06 +0100

    Updated recaptchaHandler method in Signin.js

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
 
 import { signin, authenticate } from "../auth";
+import SocialLogin from "./SocialLogin";
 
 class Signin extends Component {
   constructor() {
@@ -10,6 +11,9 @@ class Signin extends Component {
       email: "",
       password: "",
       error: "",
+      redirectToReferer: false,
+      loading: false,
+      recaptcha: false
     };
   }
 
@@ -20,6 +24,34 @@ class Signin extends Component {
     this.setState({ 
       [name]: event.target.value 
     });
+  };
+
+  recaptchaHandler = e => {
+    this.setState({
+      error: ""
+    });
+    let userDay = e.target.value.toLowerCase();
+    let dayCount;
+
+    if(userDay === 'sunday') {
+      dayCount === 0;
+    } else if (userDay === "monday") {
+      dayCount = 1;
+    } else if (userDay === "tuesday") {
+      dayCount = 2;
+    } else if (userDay === "wednesday") {
+      dayCount = 3;
+    } else if (userDay === "thursday") {
+        dayCount = 4;
+    } else if (userDay === "friday") {
+      dayCount = 5;
+    } else if (userDay === "saturday") {
+      dayCount = 6;
+    }
+
+    if(dayCount === new Date().getDay()) {
+      
+    }
   };
 
   clickSubmit = event => {

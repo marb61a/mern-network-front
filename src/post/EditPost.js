663c3743a193c5cbf3a513d0f84bb8ef9dc2a562
marb61a <marb61a@gmail.com>
2019-05-16 17:22:46 +0100

    updated NewPost render method and added clickSubmit to EditPost

--- src/post/EditPost.js
+++ src/post/EditPost.js
@@ -47,7 +47,22 @@ class EditPost extends Component {
       error: ""
     });
 
+    const value = name === "photo" ? 
+      event.target.files[0] : 
+      event.target.value;
+
+    const fileSize = name === "photo" ? event.target.files[0].size : 0;
+    this.postData.set(name, value);
+    this.setState({ [name]: value, fileSize });
   };
+
+  clickSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+    
+  }
 
   editPostForm = (title, body) => (
     <form>
--- src/post/NewPost.js
+++ src/post/NewPost.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 import { isAuthenticated } from "../auth";
 import { create } from "./apiPost";
-import { Redirect } from "react-router-dom";
 
 class NewPost extends Component {
   constructor() {
@@ -146,9 +146,24 @@ class NewPost extends Component {
     return(
       <div className="container">
         <h2 className="mt-5 mb-5">Create a new post</h2>
-        <div>
-        
+        <div
+          className="alert alert-danger"
+          style={{ display: error ? "" : "none" }}
+        >
+          {error}
         </div>
+        {
+          loading ? (
+            <div className="jumbotron text-center">
+              <h2>Loading.... </h2>
+            </div>
+          ) : (
+            ""
+          )
+        }
+        {
+          this.newPostForm(title, body)
+        }
       </div>
     );
   }

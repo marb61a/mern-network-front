fa8269015b260044c504f3f9362462b89d169cb1
marb61a <marb61a@gmail.com>
2019-05-17 20:46:44 +0100

    added apiUser unfollow and findPeople  methods

--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -67,3 +67,34 @@ export const follow = (userId, token, followId) => {
     })
     .catch(err => console.log(err));
 };
+
+export const unfollow = (userId, token, unfollowId) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/user/unfollow`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({ userId, unfollowId })
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const findPeople = (userId, token) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/user/findpeople/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};

be8b2ffc0f99ddc58d847b24b656997c65916eab
marb61a <marb61a@gmail.com>
2019-05-17 21:15:40 +0100

    added auth index.js resetPassword method

--- src/auth/index.js
+++ src/auth/index.js
@@ -84,3 +84,19 @@ export const forgotPassword = email => {
   })
   .catch(err => console.log(err));
 };
+
+export const resetPassword = resetInfo => {
+  return fetch(`${process.env.REACT_APP_API_URL}/reset-password/`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(resetInfo)
+  })
+    .then(response => {
+      console.log("forgot password response: ", response);
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};

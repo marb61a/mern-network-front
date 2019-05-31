4407a71d060bd3e185c150f793aca4de464ca9e4
marb61a <marb61a@gmail.com>
2019-05-31 20:12:10 +0100

    updated core menu render method

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -79,7 +79,27 @@ const Menu = ({history}) => (
       {
         isAuthenticated() && (
           <>
-          
+            <li className="nav-item">
+              <Link
+                to={`/findpeople`}
+                style={isActive(history, `/findpeople`)}
+                className="nav-link"
+              >
+                Find People
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to={`/user/${isAuthenticated().user._id}`}
+                style={isActive(
+                  history,
+                  `/user/${isAuthenticated().user._id}`
+                )}
+                className="nav-link"
+              >
+              {`${isAuthenticated().user.name}'s profile`}
+            </Link>
+          </li>
           </>
         )
       }
